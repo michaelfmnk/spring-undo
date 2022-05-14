3b324d2dd4fca7d447a1573e3bb09e17df3abd8d
Michael Fomenko <michael.fmnk@gmail.com>
2022-05-15 01:43:41 +0300

    changed RedisEventRecorder to use opsForValue

--- spring-undo-redis/src/main/java/dev/fomenko/springundoredis/RedisEventRecorder.java
+++ spring-undo-redis/src/main/java/dev/fomenko/springundoredis/RedisEventRecorder.java
@@ -6,35 +6,35 @@ import dev.fomenko.springundocore.dto.ActionRecord;
 import dev.fomenko.springundocore.service.EventRecorder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.apachecommons.CommonsLog;
-import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 
 @CommonsLog
 @RequiredArgsConstructor
 public class RedisEventRecorder implements EventRecorder {
-
-    private static final String HASH_NAME = "recoverable-actions-hash-name";
+    private static final String KEY_PREFIX = "recoverable-actions-hash-name-";
     private final StringRedisTemplate stringRedisTemplate;
     private final ObjectMapper objectMapper;
+
 
     public void saveEventRecord(String recordId, ActionRecord<?> actionRecord) {
         try {
             String serializedRecord = serializeRecord(actionRecord);
-            stringRedisTemplate.opsForHash().put(HASH_NAME, recordId, serializedRecord);
+            stringRedisTemplate.opsForValue().set(KEY_PREFIX + recordId, serializedRecord);
         } catch (IOException e) {
             throw new RuntimeException("Couldn't serialize an action", e);
         }
     }
 
     public Optional<ActionRecord<?>> getByRecordId(String recordId) {
-        String serializedRecord = (String) stringRedisTemplate.opsForHash().get(HASH_NAME, recordId);
+        String serializedRecord = stringRedisTemplate.opsForValue().get(KEY_PREFIX + recordId);
         if (Objects.isNull(serializedRecord)) {
             return Optional.empty();
         }
@@ -48,21 +48,27 @@ public class RedisEventRecorder implements EventRecorder {
 
     @Override
     public List<ActionRecord<?>> getAllRecords() {
-        HashOperations<String, Object, Object> opsForHash = stringRedisTemplate.opsForHash();
-        List<ActionRecord<?>> resultRecords = new ArrayList<>();
-        for (Map.Entry<Object, Object> entry : opsForHash.entries(HASH_NAME).entrySet()) {
-            try {
-                resultRecords.add(deserializeRecord((String) entry.getValue()));
-            } catch (ClassCastException | IOException e) {
-                opsForHash.delete(HASH_NAME, entry.getKey());
+        ArrayList<ActionRecord<?>> records = new ArrayList<>();
+
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        Set<String> keys = stringRedisTemplate.keys(KEY_PREFIX + "*");
+
+        if (keys != null) {
+            for (String key : keys) {
+                try {
+                    String serializedRecord = ops.get(key);
+                    records.add(deserializeRecord(serializedRecord));
+                } catch (ClassCastException | IOException e) {
+                    log.error("there was an error during serialization", e);
+                }
             }
         }
-        return resultRecords;
+
+        return records;
     }
 
     public boolean deleteByRecordId(String recordId) {
-        Long deletedCount = stringRedisTemplate.opsForHash().delete(HASH_NAME, recordId);
-        return Objects.equals(deletedCount, 1L);
+        return Boolean.TRUE.equals(stringRedisTemplate.delete(KEY_PREFIX + recordId));
     }
 
     private String serializeRecord(ActionRecord<?> recordEntity) throws JsonProcessingException {
