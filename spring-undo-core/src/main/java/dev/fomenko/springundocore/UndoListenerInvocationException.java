7b96ca668b35727ec568cf04891c4419e63638e7
Michael Fomenko <michael.fmnk@gmail.com>
2022-05-23 21:11:23 +0300

    fixed lift issue

--- spring-undo-core/src/main/java/dev/fomenko/springundocore/UndoListenerInvocationException.java
+++ spring-undo-core/src/main/java/dev/fomenko/springundocore/UndoListenerInvocationException.java
@@ -5,11 +5,15 @@ import lombok.Getter;
 import java.util.Collection;
 
 public class UndoListenerInvocationException extends RuntimeException {
-    @Getter
     private final Collection<Throwable> causes;
+
 
     public UndoListenerInvocationException(String msg, Collection<Throwable> causes) {
         super(msg, causes.iterator().next());
         this.causes = causes;
+    }
+
+    public Collection<Throwable> getCauses() {
+        return causes;
     }
 }
